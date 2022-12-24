f1482d495cc105ca5d4a8f1e599f3d4cc4a758af
nk-long <lambenitin@gmail.com>
2022-12-24 22:12:15 +0530

    first commit day one assignment

---
+++ Day1_24-12/Html/index3.js
@@ -0,0 +1,8 @@
+       const heading = React.createElement("h1",{}, "Namste React");
+
+       const root = ReactDOM.createRoot( document.getElementById("root") );
+
+       root.render(heading);
+
+    
+
